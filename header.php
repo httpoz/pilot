bfc500c37e29ba140a9652c72919c4ca9deff3a9
Oscar Mwanandimai <oscar.mwana@gmail.com>
2016-06-15 10:42:26 +0200

    added favicon

--- header.php
+++ header.php
@@ -14,8 +14,7 @@
   </title>
   <link rel="profile" href="http://gmpg.org/xfn/11"/>
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>"/>
-  <link rel="shortcut icon" href="<?php echo IMAGES. 'favicon.ico'; ?>" type="image/x-icon">
-  <link rel="icon" href="<?php echo images . 'favicon.ico'; ?>" type="image/x-icon">
+  <link rel="shortcut icon" href="<?php echo images. 'favicon.ico'; ?>" type="image/x-icon">
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
