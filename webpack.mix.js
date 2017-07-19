16fe0ec4594c7a07895275b1810e03cf0132cb15
Oscar Mwanandimai <oscar.mwana@gmail.com>
2017-07-19 12:59:16 +0200

    fixed mix resource and public paths

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,9 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-    .sass('resources/assets/sass/app.scss', 'public/css')
-    .sass('resources/assets/sass/dashboard.scss', 'public/css');
+mix.setPublicPath('public')
+    .setResourceRoot('resources');
+
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/dashboard.scss', 'public/css');
